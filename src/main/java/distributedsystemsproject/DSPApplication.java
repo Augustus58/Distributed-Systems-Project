8573e6a544fcfcbf86567c208e230dde4fd0d3b7
Aku Silvennoinen <augustus58@gmail.com>
2017-01-23 15:38:39 +0200

    Spring Boot project initialized

---
+++ src/main/java/distributedsystemsproject/DSPApplication.java
@@ -0,0 +1,12 @@
+package distributedsystemsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSPApplication {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(DSPApplication.class, args);
+  }
+}
